09edc4659931e27a07ff3e6346c6c1afb282ef7b
meonal <meonalwater@gmail.com>
2020-08-28 23:56:42 +0900

    change pub use

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,6 @@
 // re-export frequently used std items
 pub use std::cmp::{max, min, Reverse};
-pub use std::collections::{BTreeMap, BTreeSet, VecDeque};
+pub use std::collections::*;
 pub use std::{i128, i16, i32, i64, i8, isize, u128, u16, u32, u64, u8, usize};
 
 pub use itertools::Itertools;
@@ -14,8 +14,8 @@ pub use proconio::marker::{Bytes, Chars, Isize1, Usize1};
 
 // num stuffs
 pub use num::complex::Complex;
-pub use num::integer::{gcd, lcm};
-pub use num::{abs, clamp, BigInt, BigRational, BigUint, Num, Rational};
+pub use num::integer::*;
+pub use num::*;
 
 // comprehension
 pub use comprehension::*;
