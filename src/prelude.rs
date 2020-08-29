4b25b5a5b9f560b7282da9726e4686804089d5e2
meonal <meonalwater@gmail.com>
2020-08-29 16:26:12 +0900

    change pub use

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 // re-export frequently used std items
-pub use std::cmp::{max, min, Reverse};
+pub use std::cmp::*;
 pub use std::collections::*;
 pub use std::{i128, i16, i32, i64, i8, isize, u128, u16, u32, u64, u8, usize};
 
